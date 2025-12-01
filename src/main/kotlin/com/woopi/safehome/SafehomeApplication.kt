7d3caff99b6b08cb42bfb3b927997a8d0608ba93
woopi1030 <lozenkrantz@gmail.com>
2025-12-02 01:20:59 +0900

    init commit

---
+++ src/main/kotlin/com/woopi/safehome/SafehomeApplication.kt
@@ -0,0 +1,11 @@
+package com.woopi.safehome
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SafehomeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SafehomeApplication>(*args)
+}
---
+++ src/test/kotlin/com/woopi/safehome/SafehomeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.woopi.safehome
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SafehomeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
